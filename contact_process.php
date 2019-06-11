24c7598dd503ee2284db9c78535e7b485694a3d3
fahmi prasanda <fahmiprsanda170698>
2019-06-11 14:58:41 +0700

    fix error
    fix error

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "fahmiprasanda5@gmail.com";
+    $to = "fahmiprasanda@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
